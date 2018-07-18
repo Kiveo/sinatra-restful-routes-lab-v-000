15c6bbcbdf3ac1b3e6c42826de6e0b51e7c340c2
Maverick Tesini <mavericktesini@gmail.com>
2018-07-18 03:28:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < Sinatra::Base
   get '/recipes/new' do
     erb :new
   end
-  
-  #CREATE ACTION 
+
+  #CREATE ACTION
   post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     @recipe.save
